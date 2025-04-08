6616306df3964d73ad65c61c3c5474babbfe5941
l-phan <l-phan@creationline.com>
2025-04-09 08:52:09 +0900

    develop api gateway

---
+++ src/test/java/com/loc/microservices/api_gateway_service_spring_boot/ApiGatewayServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loc.microservices.api_gateway_service_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
